1f00b2dfa2ba66b35c37dada3ab3004819ea9361
Nicco Ryan <niccoryan0@gmail.com>
2020-07-07 12:41:44 -0700

    Created gist of UserInterface method

--- Lab02-UnitTests/Lab02-UnitTests/Program.cs
+++ Lab02-UnitTests/Lab02-UnitTests/Program.cs
@@ -6,7 +6,48 @@ namespace Lab02_UnitTests
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            UserInterface();
+        }
+
+        static void UserInterface()
+        {
+            Console.WriteLine("Hello and welcome to N&R Banking Inc.!");
+            Console.WriteLine("How can we help ya today?");
+            int choice;
+            do
+            {
+                string options = @"1. Withdraw
+                2. Deposit
+                3. View Balance
+                4. Exit";
+                Console.WriteLine(options);
+                choice = int.Parse(Console.ReadLine());
+                string repeat = "y";
+                switch (choice)
+                {
+                    case 1:
+                        // Withdraw();
+                        break;
+                    case 2:
+                        // Deposit();
+                        break;
+                    case 3:
+                        // ViewBalance();
+                        break;
+                    case 4:
+                        repeat = "n";
+                        break;
+                }
+                if(repeat == "y")
+                {
+                    Console.WriteLine("Choose another option? y/n");
+                    repeat = Console.ReadLine();
+                }
+                if (repeat == "n")
+                {
+                    break;
+                }
+            } while (choice != 4);
         }
     }
 }
